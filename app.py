426ab9e8e1ee6ee2d17ed5465df5a5f91a1251d3
kkarakk <kkarakk@gmail.com>
2014-05-16 17:52:12 +0530

    Added REST PUT functionality

--- app.py
+++ app.py
@@ -52,6 +52,29 @@ def create_task():
     tasks.append(task)
     return jsonify({'task':task}),201
 
+@app.route('/todo/api/v1.0/tasks/<int:task_id>',methods = ['PUT'])
+def update_task(task_id):
+    task=filter(lambda t:t['id'] == task_id,tasks)
+    if(len(task)==0):
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title'])!=unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description'])!=unicode:
+        abort(400)
+    if 'time' in request.json and type(request.json['time'])!=unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done'])is not bool:
+        abort(400)
+    task[0]['title']=request.json.get('title',task[0]['title'])
+    task[0]['description']=request.json.get('description',task[0]['description'])
+    task[0]['time']=request.json.get('time',task[0]['time'])
+    task[0]['done']=request.json.get('done',task[0]['done'])
+    return jsonify({'task':task[0]})
+    
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
     
